5d9386f4b018678746a4987985bd8b098b8988c1
lee Helen <dzzzzzb@gmail.com>
2020-12-03 20:13:07 +0800

    sockit.io

---
+++ index.js
@@ -0,0 +1,10 @@
+let app = require('express')();
+let http = require('http').createServer(app);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname  + '/index.html');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000, Conntetd done');
+});

b58ba24d4c9a813c412ab81b7e4682f4c9f8ab67
lee Helen <dzzzzzb@gmail.com>
2020-12-03 20:30:54 +0800

    vefore vue

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 let app = require('express')();
 let http = require('http').createServer(app);
+let io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname  + '/index.html');
 });
 
+
 http.listen(3000, () => {
   console.log('listening on *:3000, Conntetd done');
 });
+
+
+io.on('connection',(socket)=>{
+  console.log('there is a connetion!');
+  socket.on('disconnect', ()=>{
+    console.log('user disconnnnnnnnect!');
+  })
+})
